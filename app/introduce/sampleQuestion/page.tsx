901c6fbaf12d878662084059b8283b6292366e72
thekanon <dlengjs12@naver.com>
2023-12-18 03:18:04 +0900

    빌드 오류 수정

--- app/introduce/sampleQuestion/page.tsx
+++ app/introduce/sampleQuestion/page.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-
-import ProgressBar from '@/app/ui/common/Atoms/ProgressBar';
-import IconQuestion from '@/app/ui/questions/IconQuestion';
-import ButtonComponent from '@/app/ui/common/Atoms/ButtonComponent';
+import { Question } from '@/app/lib/definitions';
 
 import QuestionContainer from '@/app/ui/questions/QuestionContainer';
 
 import { getQuestionsWithOptions } from '@/app/lib/data';
 
-const SampleQuestionPage = async ({ question = 0 }: { question: number }) => {
-  const questions = await getQuestionsWithOptions();
+const SampleQuestionPage = async () => {
+  // const questions = (await getQuestionsWithOptions()) as Question[];
+  const queryResultRow = await getQuestionsWithOptions();
+  function isQuestion(obj: any): obj is Question {
+    return 'question_id' in obj && 'title' in obj;
+  }
+
+  const questions = queryResultRow.filter(isQuestion);
 
   return (
     <div>
