555afda3cbba99af9b54758af0cf55187e9ff1b4
thekanon <dlengjs12@naver.com>
2023-12-19 14:20:12 +0900

    코드 수정

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,20 @@
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Page() {
   const router = useRouter();
-  if (!localStorage?.getItem('visited')) {
-    // localStorage.setItem('visited', 'true');
-    router.push('/introduce');
-  } else {
-    // 사용자가 이전에 방문한 적이 있음을 의미
-    // 메인 페이지나 다른 페이지를 보여주는 로직
-  }
+
+  useEffect(() => {
+    if (localStorage && !localStorage?.getItem('visited')) {
+      // localStorage.setItem('visited', 'true');
+      router.push('/introduce');
+    } else {
+      // 사용자가 이전에 방문한 적이 있음을 의미
+      // 메인 페이지나 다른 페이지를 보여주는 로직
+    }
+  }, []);
 
   return <div></div>;
 }
