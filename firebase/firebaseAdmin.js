527c139543afa9cc3737507fc12e8f0a628f01e8
thekanon <dlengjs12@naver.com>
2024-02-10 19:55:26 +0900

    TSK-29 .env 개선

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -1,7 +1,14 @@
 // src/firebase/firebaseAdmin.ts
 
 import * as admin from 'firebase-admin';
-import * as serviceAccount from './serviceAccountKey.json';
+import * as serviceAccountData from './serviceAccountKey.json';
+
+const serviceAccount = {
+  ...serviceAccountData,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 if (!admin.apps.length)
   admin.initializeApp({
